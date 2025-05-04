d37e788db694cf263de2885e87d5d8b8d5ea71fd
yazan <yazanshrouf149@gmail.com>
2025-05-04 16:47:43 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui_samples/samples/paypal.dart';
 
 void main() {
-  runApp(const PayPageDemo()
-
-  );
+  runApp(const PayPageDemo());
 }
